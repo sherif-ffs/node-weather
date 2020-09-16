e943e7f889efd4ef01276cc0c1fc740061b9cfb5
Sherif Elmetwally <sge5@njit.edu>
2020-09-16 15:11:44 -0400

    abstract geocode http call

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
-const url = 'http://api.weatherstack.com/current?access_key=428a5730d31ad6107b849a0e0a40b8e9&query=37.8267,-122.4233'
+// const url = 'http://api.weatherstack.com/current?access_key=428a5730d31ad6107b849a0e0a40b8e9&query=40.7178,-74.0431&units=f'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log('data: ', data.current)
+// request({url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('unable to connect to weather service')
+
+//     } else if (response.body.error) {
+//         console.log('unable to find location')
+//     }
+//     else {
+//         const data = response.body.current
+//         const currentTemp = data.temperature
+//         const chanceOfRain = data.precip
+//         console.log(`It is currently ${currentTemp} degrees, there is a ${chanceOfRain}% of rain`)    }
+// })
+
+geocode('Boston', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
