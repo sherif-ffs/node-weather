9ba28e910adbd37fedbd62ebfada0c04e26d942f
Sherif Elmetwally <sge5@njit.edu>
2020-09-16 15:59:45 -0400

    abstract forecast call

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'http://api.weatherstack.com/current?access_key=428a5730d31ad6107b849a0e0a40b8e9&query=40.7178,-74.0431&units=f'
-
-// request({url: url, json: true}, (error, response) => {
-//     if (error) {
-//         console.log('unable to connect to weather service')
-
-//     } else if (response.body.error) {
-//         console.log('unable to find location')
-//     }
-//     else {
-//         const data = response.body.current
-//         const currentTemp = data.temperature
-//         const chanceOfRain = data.precip
-//         console.log(`It is currently ${currentTemp} degrees, there is a ${chanceOfRain}% of rain`)    }
-// })
+forecast(40.7178,-74.0431, (error, data) => {
+    console.log('Error: ', error)
+    console.log('current temperature: ', data.temperature)
+    console.log('chance of rain: ', data.precip)
+})
 
 geocode('Boston', (error, data) => {
     console.log('Error: ', error)
